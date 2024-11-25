864685749d491494bf3345d0ec584d1d8ea2acd2
Brendan Armstrong <brendanarmstrong@proton.me>
2024-11-24 22:33:09 -0500

    updating screenshot script

--- scripts/system/screen_to_clipboard.sh
+++ scripts/system/screen_to_clipboard.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-scrot '/tmp/%F_%T_$wx$h.png' --line mode=edge --select -e 'xclip -selection clipboard -target image/png -i $f'
+scrot '/tmp/%F_%T_$wx$h.png' --line mode=classic --select -e 'xclip -selection clipboard -target image/png -i $f'
 #scrot --line mode=edge --select -F - | 
 #| xclip -selection clipboard -t image/png
