99b4067af17fd75c3ed4b549c84312252d977043
Brendan Armstrong <brendanarmstrong@protonmail.com>
2025-05-27 20:04:50 -0400

    add some things to yazi

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -1,16 +1,3 @@
-function Status:name()
-	local h = cx.active.current.hovered
-	if not h then
-		return ui.Span("")
-	end
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-
-	return ui.Span(" " .. h.name .. linked)
-end
-
 Status:children_add(function(self)
 	local h = self._current.hovered
 	if h and h.link_to then
