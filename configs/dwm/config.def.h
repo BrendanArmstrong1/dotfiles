52b5b72581ce029e38c77dec19fd28d88dfcb74b
Brendan Armstrong <brendanarmstrong@protonmail.com>
2025-06-08 17:03:29 -0400

    changed colorscheme and added defines

--- configs/dwm/config.def.h
+++ configs/dwm/config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+	[SchemeHid]  = { col_cyan,  col_gray1, col_gray1  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
