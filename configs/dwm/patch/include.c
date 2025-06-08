92e313fc2cb98d048000fd8a04fa07c05d70d7d0
Brendan Armstrong <brendanarmstrong@protonmail.com>
2025-06-08 10:45:56 -0400

    add patch bakkeby/dwm-barmodules-awesomebar-6.3.diff

--- configs/dwm/patch/include.c
+++ configs/dwm/patch/include.c
@@ -2,7 +2,7 @@
 #include "bar_ltsymbol.c"
 //#include "bar_status.c"
 #include "bar_tags.c"
-#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
 #include "bar_status2d.c"
 #include "bar_dwmblocks.c"
 #include "bar_statuscmd.c"
